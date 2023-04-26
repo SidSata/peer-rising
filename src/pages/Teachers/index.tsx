a135dc1c903b859a139eb66b27bd2edcf4fde7c4
Siddhant Satapathy <siddhant.satapathy1@gmail.com>
2023-04-26 14:34:03 -0700

    Ready to deploy ig

--- src/pages/Teachers/index.tsx
+++ src/pages/Teachers/index.tsx
@@ -1,13 +1,10 @@
 import { lazy } from "react";
-import TeacherContent from "../../content/TeacherContent.json";
 import CurriculumCreatorContent from "../../content/CurriculumCreatorContent.json";
 import { Divider } from 'antd';
-// import { EmailInput } from "../../components/CurriculumForm/styles";
 
 const SplitContainer = lazy(() => import("../../common/SplitContainer"));
 const ScrollToTop = lazy(() => import("../../common/ScrollToTop"));
 const Block = lazy(() => import("../../components/Block"));
-const CurriculumForm = lazy(() => import("../../components/CurriculumForm"));
 const UserField = lazy(() => import("../../components/UserField"));
 
 
