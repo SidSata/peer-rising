76043467c319771f88ec7f8be42b9952506e566e
Siddhant Satapathy <siddhant.satapathy1@gmail.com>
2023-05-01 20:14:55 -0700

    Contact form responsiveness

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -1,28 +1,106 @@
 import { Row, Col } from "antd";
 import { withTranslation } from "react-i18next";
-import { Slide, Zoom } from "react-awesome-reveal";
+import { Slide } from "react-awesome-reveal";
 import { ContactProps, ValidationTypeProps } from "./types";
 import { useForm } from "../../common/utils/useForm";
 import validate from "../../common/utils/validationRules";
-import { Button } from "../../common/Button";
 import Block from "../Block";
-import Input from "../../common/Input";
-import TextArea from "../../common/TextArea";
+
 import { ContactContainer, FormGroup, Span, ButtonContainer } from "./styles";
+
+
+import { useState } from 'react';
+import { Form, Input, Button, notification, message } from 'antd';
+
+interface FormData {
+  name: string;
+  email: string;
+  message: string;
+}
+
+// interface ContactFormProps {}
+
+// const ContactForm: React.FC<ContactFormProps> = () => {
+  
+
+//   return (
+    
+//   );
+// };
+
+// export default ContactForm;
+
 
 const Contact = ({ title, content, id, t }: ContactProps) => {
   const { values, errors, handleChange, handleSubmit } = useForm(
     validate
   ) as any;
 
-  const ValidationType = ({ type }: ValidationTypeProps) => {
-    const ErrorMessage = errors[type];
-    return (
-      <Zoom direction="left">
-        <Span erros={errors[type]}>{ErrorMessage}</Span>
-      </Zoom>
-    );
+  const [form] = Form.useForm();
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const onFinish = async (values: FormData) => {
+    setLoading(true);
+    try {
+      fetch('https://8e4e-142-254-1-167.ngrok-free.app/contact', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ values }),
+      })
+      .then(response => {
+        if (response.ok) {
+          console.log('Contact submitted successfully!');
+          setSuccess(true);
+          form.resetFields();
+          message.success('Your message has been sent successfully!');
+
+          
+        } else {
+          console.log('Error submitting contact');
+          message.error('Contact submitted successfully!');
+        }
+      })
+      .catch(error => {
+        console.error('Error submitting Contact:', error);
+      });
+
+      notification.success({
+        message: 'Success',
+        description: 'Message sent successfully!',
+      });
+      form.resetFields();
+
+    } catch (error) {
+      console.error('Error sending message: ', error);
+      notification.error({
+        message: 'Error',
+        description: 'Failed to send message. Please try again later.',
+      });
+    } finally {
+      setLoading(false);
+    }
   };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.error('Failed:', errorInfo);
+    notification.error({
+      message: 'Validation error',
+      description: 'Please fill in all the required fields.',
+    });
+  };
+  
+
+  // const ValidationType = ({ type }: ValidationTypeProps) => {
+  //   const ErrorMessage = errors[type];
+  //   return (
+  //     <Zoom direction="left">
+  //       <Span erros={errors[type]}>{ErrorMessage}</Span>
+  //     </Zoom>
+  //   );
+  // };
 
   return (
     <ContactContainer id={id}>
@@ -33,42 +111,47 @@ const Contact = ({ title, content, id, t }: ContactProps) => {
           </Slide>
         </Col>
         <Col lg={12} md={12} sm={24} xs={24}>
-          <Slide direction="right">
-            <FormGroup autoComplete="off" onSubmit={handleSubmit}>
-              <Col span={24}>
-                <Input
-                  type="text"
-                  name="name"
-                  placeholder="Your Name"
-                  value={values.name || ""}
-                  onChange={handleChange}
-                />
-                <ValidationType type="name" />
-              </Col>
-              <Col span={24}>
-                <Input
-                  type="text"
-                  name="email"
-                  placeholder="Your Email"
-                  value={values.email || ""}
-                  onChange={handleChange}
-                />
-                <ValidationType type="email" />
-              </Col>
-              <Col span={24}>
-                <TextArea
-                  placeholder="Your Message"
-                  value={values.message || ""}
-                  name="message"
-                  onChange={handleChange}
-                />
-                <ValidationType type="message" />
-              </Col>
-              <ButtonContainer>
-                <Button name="submit">{t("Submit")}</Button>
-              </ButtonContainer>
-            </FormGroup>
-          </Slide>
+          <Form
+        layout="vertical"
+        name="basic"
+        form={form}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label="Name"
+          name="name"
+          rules={[{ required: true, message: 'Please input your name!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            { required: true, message: 'Please input your email!' },
+            { type: 'email', message: 'Please enter a valid email!' },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Message"
+          name="message"
+          rules={[{ required: true, message: 'Please input your message!' }]}
+        >
+          <Input.TextArea rows={4} />
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" loading={loading}>
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
         </Col>
       </Row>
     </ContactContainer>
--- src/scripts/server.js
+++ src/scripts/server.js
@@ -104,6 +104,63 @@ app.post('/submit', cors(corsOptions), (req, res) => {
   }
 });
 
+app.post('/contact', cors(corsOptions), (req, res) => {
+  const { name, email, message } = req.body;
+  console.log('Received:', name, email, message);
+
+  // Read the curriculum file based on the selected subject and grade level
+  // const filePath = `/Users/siddhant/Desktop/Berkeley Classes/Spring 2023/peer-rising/peer-rising/peer-rising/sample_curricula/${subject}_${gradeLevel}.txt`;
+  // const curriculum = fs.readFileSync(filePath, "utf-8");
+  // const request = new Request({ subject, gradeLevel, email });
+  // let error = null;
+
+  // request.save().then(() => {
+  //   console.log('Request saved to database');
+  //   // send email to user
+  //   // res.sendStatus(200);
+  // }).catch((err) => {
+  //   console.error('Error saving request to database:', err);
+  //   error = err.message;
+  //   // res.sendStatus(500);
+  // });
+
+  // if (error) {
+  //   // Send an error response with the error message
+  //   res.status(500).json({ error });
+  // }
+  // // create email message
+  // const message = {
+  //   from: process.env.EMAIL,
+  //   to: email,
+  //   subject: "Your curriculum",
+  //   text: `Here is your curriculum for ${subject} in ${gradeLevel}:\n\n${curriculum}`,
+  //   attachments: [
+  //     {
+  //       filename: `${subject}_${gradeLevel}.txt`,
+  //       content: curriculum,
+  //     },
+  //   ],
+  // };
+
+  // // send email
+  // transporter.sendMail(message, (err, info) => {
+  //   if (err) {
+  //     console.log(err);
+  //     error = err.message
+  //     // res.status(500).send('Error sending email');
+  //   } else {
+  //     console.log('Email sent: ' + info.response);
+  //     // res.status(200).send('Email sent successfully');
+  //   }
+  // });
+  // if (!error) {
+  // res.sendStatus(200);
+  // } else {
+  //   res.sendStatus(500);
+  // }
+  res.sendStatus(200);
+});
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
