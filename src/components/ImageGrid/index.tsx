98b8a5f60af9e869485a44d57d311ccf1dcc044c
Siddhant Satapathy <siddhant.satapathy1@gmail.com>
2023-05-01 19:39:43 -0700

    Netlify shenanigans

--- src/components/ImageGrid/index.tsx
+++ src/components/ImageGrid/index.tsx
@@ -5,16 +5,16 @@ const ImageGrid: React.FC = () => {
   return (
     <Row gutter={[16, 16]} style={{ padding: '16px 0' }}>
       <Col xs={24} sm={12}>
-        <img src="img/customized/1.png" width="100%" height="100%"/>
+        <img src="img/customized/1.png" alt="" width="100%" height="100%"/>
       </Col>
       <Col xs={24} sm={12}>
-        <img src="img/customized/2.png"  width="100%" height="100%"/>
+        <img src="img/customized/2.png" alt="" width="100%" height="100%"/>
       </Col>
       <Col xs={24} sm={12}>
-        <img src="img/customized/3.png"  width="100%" height="100%"/>
+        <img src="img/customized/3.png" alt="" width="100%" height="100%"/>
       </Col>
       <Col xs={24} sm={12}>
-        <img src="img/customized/4.png"  width="100%" height="100%"/>
+        <img src="img/customized/4.png" alt="" width="100%" height="100%"/>
       </Col>
     </Row>
   );
